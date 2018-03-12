991507f126e188dcecb1cf5a3f56c470d87be911
Guivarela <guivarela7@hotmail.com>
2018-03-11 21:07:03 -0300

    commit do projeto

---
+++ app/src/main/java/deswebmob/usjt/br/geodata/DetalhePaisActivity.java
@@ -0,0 +1,28 @@
+package deswebmob.usjt.br.geodata;
+
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import deswebmob.usjt.br.geodata.entity.Pais;
+
+/**
+ * Created by Guilherme Varela, RA: 81613746.
+ */
+
+public class DetalhePaisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe_pais);
+        TextView txtPais = (TextView)findViewById(R.id.txtPais);
+        Intent intent = getIntent();
+        Pais pais = (Pais)intent.getSerializableExtra(ListaPaisesActivity.PAIS);
+        txtPais.setText(pais.toString());
+
+    }
+}
---
+++ app/src/main/java/deswebmob/usjt/br/geodata/ListaPaisesActivity.java
@@ -0,0 +1,58 @@
+package deswebmob.usjt.br.geodata;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import deswebmob.usjt.br.geodata.entity.Data;
+import deswebmob.usjt.br.geodata.entity.Pais;
+
+/**
+ * Created by Guilherme Varela, RA: 81613746.
+ */
+
+public class ListaPaisesActivity extends AppCompatActivity {
+    public static final String PAIS = "deswebmob.usjt.br.geodata.pais";
+    Activity atividade;
+    ArrayList<Pais> paises;
+    ArrayList<String> nomes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_paises);
+        atividade = this;
+        Intent intent = getIntent();
+        String continente = intent.getStringExtra(MainActivity.CHAVE);
+        paises = Data.listarPaises(continente);
+        nomes = Data.listarNomes(paises);
+
+        ListView listView = (ListView) findViewById(R.id.lista_paises);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, nomes);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // manda para a tela de detalhe
+                Intent intent = new Intent(atividade, DetalhePaisActivity.class);
+                intent.putExtra(PAIS, paises.get(position));
+
+                startActivity(intent);
+
+            }
+
+        });
+    }
+}
